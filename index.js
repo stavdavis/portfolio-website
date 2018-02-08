f72bc4273cb64e35b15f041e5208cdc5ba622d21
Stav Davis <stav.davis@gmail.com>
2018-02-07 20:15:30 -0800

    initialized project; added html, css and js (jQuery) files; added navbar, using bootstrap

---
+++ index.js
@@ -0,0 +1,16 @@
+// Scrolling animation (velocity is faster than jQuery's "animate")
+$(".about-nav").click(function () {
+    $("#about-section").velocity("scroll", 1000);
+});
+
+$(".web-dev-nav").click(function () {
+    $("#web-dev-section").velocity("scroll", 1000);
+});
+
+$(".data-sci-nav").click(function () {
+    $("#data-sci-section").velocity("scroll", 1000);
+});
+
+$(".contact-nav").click(function () {
+    $("#contact-section").velocity("scroll", 1000);
+});
