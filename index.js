0384ad4b5c7eb82466603c4b98a41fca3bd6f152
Stav Davis <stav.davis@gmail.com>
2018-02-09 12:32:01 -0800

    added portfolio screenshots; fixed navbar links

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 // Scrolling animation (velocity is faster than jQuery's "animate")
+$(".navbar-brand").click(function () {
+    $("#pagetop").velocity("scroll", 1000);
+});
+
 $(".about-nav").click(function () {
     $("#about-section").velocity("scroll", 1000);
 });
 
-$(".web-dev-nav").click(function () {
-    $("#web-dev-section").velocity("scroll", 1000);
-});
-
-$(".data-sci-nav").click(function () {
-    $("#data-sci-section").velocity("scroll", 1000);
+$(".portfolio-nav").click(function () {
+    $("#portfolio-section").velocity("scroll", 1000);
 });
 
 $(".contact-nav").click(function () {
     $("#contact-section").velocity("scroll", 1000);
-});
-
-$(".navbar-brand").click(function () {
-    $("#pagetop").velocity("scroll", 1000);
 });
 
 // $(".navbar-option").on('click', function(){
